c7c4d240d1a0f6fd4e1d6562d7754b3e8a88c4f3
naglenda31 <naglenda31@gmail.com>
2021-03-26 01:20:39 +0000

    added new classes for digits and clock

--- src/js/component/counter.js
+++ src/js/component/counter.js
@@ -6,13 +6,20 @@ import { faClock } from "@fortawesome/free-regular-svg-icons";
 
 export function SimpleCounter(props) {
 	return (
-		<div className="counter d-flex flex-row align-items-center justify-content-center">
-			<FontAwesomeIcon icon={faClock} />
-			<div className="digit">{props.five % 10}</div>
-			<div className="digit">{props.four % 10}</div>
-			<div className="digit">{props.three % 10}</div>
-			<div className="digit">{props.two % 10}</div>
-			<div className="digit">{props.one % 10}</div>
+		<div className="counter d-flex flex-row align-items-center justify-content-between">
+			<div className="clock bg-elements">
+				<FontAwesomeIcon icon={faClock} />
+			</div>
+
+			<div className="digit bg-elements">{props.five % 10}</div>
+
+			<div className="digit bg-elements">{props.four % 10}</div>
+
+			<div className="digit bg-elements">{props.three % 10}</div>
+
+			<div className="digit bg-elements">{props.two % 10}</div>
+
+			<div className="digit bg-elements">{props.one % 10}</div>
 		</div>
 	);
 }
